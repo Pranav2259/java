8f76119a9bd0373b5ec9e5e5bce3973e38477d1e
Pranav2259 <85480256+Pranav2259@users.noreply.github.com>
2021-06-21 13:42:45 +0530

    Create Task3.java

---
+++ Task3.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class SwitchCase {
+
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+				System.out.println("Pick One: 1. Hi\t 2. Hey\t 3. Hello\t");
+				 int choice = sc.nextInt();
+		switch(choice) {
+		case 1 : System.out.println("You Said Hi");
+		break;
+		case 2 : System.out.println("You Said Hi");
+		break;
+		case 3 : System.out.println("You Said Hi");
+		break;
+		default : System.out.println("Invalid Choice");
+		}
+	}	
+}
