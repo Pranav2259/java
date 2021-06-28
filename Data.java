0964bb968aa1aa7eb2fc41a6d105257d21673da6
Pranav2259 <85480256+Pranav2259@users.noreply.github.com>
2021-06-28 21:23:00 +0530

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class Data {
+	
+	public static void main(String[] args) {
+		 
+		Employee E1 = new Employee();
+			E1.name = "Robert";
+			E1.YearOfJoining = 1994; 
+			E1.salary = "" ;
+			E1.Address= "64C-WallsStreet" ;
+			
+		Employee E2 = new Employee();
+			E2.name = "Sam";
+			E2.YearOfJoining = 2000; 
+			E2.salary = "" ;
+			E2.Address= "68D-WallsStreet" ;
+			
+		Employee E3 = new Employee();
+			E3.name = "John";
+			E3.YearOfJoining = 1999; 
+			E3.salary = "" ;
+			E3.Address= "26B-WallsStreet" ;
+	}
+
+}
